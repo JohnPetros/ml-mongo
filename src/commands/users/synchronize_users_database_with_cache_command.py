872e6a8b6b63a91776bd8f6c77e2bc78c050777e
JohnPetros <joaopedro.nc@outlook.com>
2025-04-20 12:11:59 -0300

    ✨ feat: add SynchronizeUsersDatabaseWithCache

---
+++ src/commands/users/synchronize_users_database_with_cache_command.py
@@ -0,0 +1,17 @@
+from commands.command import Command
+from commands.users.cache_users_command import CacheUsersCommand
+from repositories.users_repository import UsersRepository
+
+
+class SynchronizeUsersDatabaseWithCache(Command):
+    def __init__(self):
+        super().__init__()
+        self.command = CacheUsersCommand()
+        self.repository = UsersRepository()
+
+    def run(self):
+        users_cache = self.repository.findAll(is_cache_enable=True)
+        self.repository.removeAll(is_cache_enable=False)
+        self.repository.addMany(users_cache, is_cache_enable=False)
+        self.output.loading()
+        self.output.success("Usuários sincronizados com banco de dados!")
